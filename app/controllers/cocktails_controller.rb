f616f764fd90602fa4410e986ef962bb28e73def
Nick Hape <hape.nick@gmail.com>
2020-12-21 19:14:43 +0100

    added a delete functionality for cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,6 +4,13 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
   def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,3 @@ Rails.application.routes.draw do
   end
   resources :doses, only: [:destroy]
 end
-
-
-# Rails.application.routes.draw do
-#   root to: "cocktails#index"
-#   resources :cocktails, only: [:index, :show, :new, :create] do
-#     resources :doses, only: [:create]
-#     resources :reviews, only: [:create]
-#   end
-#   resources :doses, only: [:destroy]
-# end
