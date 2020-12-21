de953f419b5e5baacbcf8026634f66cdf5b83a1c
Nick Hape <hape.nick@gmail.com>
2020-12-21 18:55:49 +0100

    fixed the broken add dose functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails do
-    resources :doses, only: [:create]
+    resources :doses, only: [:create, :new]
   end
   resources :doses, only: [:destroy]
 end
